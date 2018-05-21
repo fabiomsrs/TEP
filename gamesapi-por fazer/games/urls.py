1640dccf98b55f283f7047ac2519effd8b1ceba5
Sywrah <sywrahgabriella@hotmail.com>
2018-05-21 17:05:10 -0300

    refactoring

--- gamesapi-por fazer/games/urls.py
+++ gamesapi-por fazer/games/urls.py
@@ -5,17 +5,17 @@ Author: Gaston C. Hillar - Twitter.com/gastonhillar
 Publisher: Packt Publishing Ltd. - http://www.packtpub.com
 """
 from django.urls import path
-from games import views
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from games.models import Game, GameCategory, Score, Player
 from games.serializers import GameSerializer, GameCategorySerializer, PlayerSerializer, ScoreSerializer
 
 urlpatterns = [
-    path('games/', views.ViewList.as_view(queryset=Game.objects.all(), serializer_class=GameSerializer), name='game-list'),
-    path('games/<int:pk>/', views.ViewDetail.as_view(queryset=Game.objects.all(), serializer_class=GameSerializer), name='game-detail'),
-    path('game-categories/', views.ViewList.as_view(queryset=GameCategory.objects.all(), serializer_class=GameCategorySerializer), name='gamecategory-list'),
-    path('game-categories/<int:pk>/', views.ViewDetail.as_view(queryset=GameCategory.objects.all(), serializer_class=GameCategorySerializer), name='gamecategory-detail'),
-    path('players/', views.ViewList.as_view(queryset=Player.objects.all(), serializer_class=PlayerSerializer), name='player-list'),
-	path('players/<int:pk>/', views.ViewDetail.as_view(queryset=Player.objects.all(), serializer_class=PlayerSerializer), name='player-detail'),
-	path('scores/', views.ViewList.as_view(queryset=Score.objects.all(), serializer_class=ScoreSerializer), name='score-list'),
-	path('scores/<int:pk>/', views.ViewDetail.as_view(queryset=Score.objects.all(), serializer_class=ScoreSerializer), name='score-detail'),
+    path('games/', ListCreateAPIView.as_view(queryset=Game.objects.all(), serializer_class=GameSerializer), name='game-list'),
+    path('games/<int:pk>/', RetrieveUpdateDestroyAPIView.as_view(queryset=Game.objects.all(), serializer_class=GameSerializer), name='game-detail'),
+    path('game-categories/', ListCreateAPIView.as_view(queryset=GameCategory.objects.all(), serializer_class=GameCategorySerializer), name='gamecategory-list'),
+    path('game-categories/<int:pk>/', RetrieveUpdateDestroyAPIView.as_view(queryset=GameCategory.objects.all(), serializer_class=GameCategorySerializer), name='gamecategory-detail'),
+    path('players/', ListCreateAPIView.as_view(queryset=Player.objects.all(), serializer_class=PlayerSerializer), name='player-list'),
+	path('players/<int:pk>/', RetrieveUpdateDestroyAPIView.as_view(queryset=Player.objects.all(), serializer_class=PlayerSerializer), name='player-detail'),
+	path('scores/', ListCreateAPIView.as_view(queryset=Score.objects.all(), serializer_class=ScoreSerializer), name='score-list'),
+	path('scores/<int:pk>/', RetrieveUpdateDestroyAPIView.as_view(queryset=Score.objects.all(), serializer_class=ScoreSerializer), name='score-detail'),
 	]
